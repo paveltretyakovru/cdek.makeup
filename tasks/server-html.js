47ab01cd4a8b17ee2b8172aaad412b0ba8e0e2be
Tretyakov Pavel <tretyakovpavel.ru@gmail.com>
2016-02-18 11:19:51 +0300

    Fixed server-html addr public path

--- tasks/server-html.js
+++ tasks/server-html.js
@@ -2,11 +2,11 @@ var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 
 gulp.task('connect', function() {
-  $.connect.server({ root: 'build', port: 4444, livereload: false });
+  $.connect.server({ root: 'public', port: 4444, livereload: false });
 });
 
 gulp.task('html', function() {
-  return gulp.src('./source/index.html')
+  return gulp.src('./public/index.html')
     .pipe($.plumber())
     .pipe(gulp.dest('./public'));
 });
