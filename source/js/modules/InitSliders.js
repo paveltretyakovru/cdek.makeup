49e4aa5a8d8f041749daa45f5bae56a222bb4317
Tretyakov Pavel <tretyakovpavel.ru@gmail.com>
2016-04-28 12:38:28 +0300

    Fixed first slider

--- source/js/modules/InitSliders.js
+++ source/js/modules/InitSliders.js
@@ -5,6 +5,14 @@ let InitSliders = () => {
     autoplay : true,
     arrows : false
   });
+
+  let $language = $('.language');
+  let $firstSlider = $('article.with-slider .unslider-nav');
+  $firstSlider.css('top', $language.position().top + 4);
+
+  $(window).resize(() => {
+    $firstSlider.css('top', $language.position().top + 4);
+  })
 }
 
 export default InitSliders;
